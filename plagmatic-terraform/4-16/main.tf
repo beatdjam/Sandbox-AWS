c86199610a50defa6dff210c9f662f63b3513578
T.Hori <beat3526@gmail.com>
2022-01-26 01:10:37 +0900

    実践terraform chapter5 5.1.2まで

---
+++ plagmatic-terraform/4-16/main.tf
@@ -0,0 +1,15 @@
+# ポリシードキュメント
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+# IAMポリシー
+resource "aws_iam_policy" "example" {
+  name = "example"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
