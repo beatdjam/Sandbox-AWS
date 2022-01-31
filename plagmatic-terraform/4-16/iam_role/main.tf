87d9312748653228fece22ec5e5127fba9c72d38
T.Hori <beat3526@gmail.com>
2022-02-01 00:31:22 +0900

    実践terraform chapter10

--- plagmatic-terraform/4-16/iam_role/main.tf
+++ plagmatic-terraform/4-16/iam_role/main.tf
@@ -35,3 +35,7 @@ resource "aws_iam_role_policy_attachment" "default" {
 output "iam_role_name" {
   value = aws_iam_role.default.name
 }
+
+output "iam_role_arn" {
+  value = aws_iam_role.default.arn
+}
