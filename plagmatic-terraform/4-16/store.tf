88182e241cd6bd298c46daf6bf083b2de605969f
T.Hori <beat3526@gmail.com>
2022-02-06 21:05:36 +0900

    実践terraform chapter13 13.1.2

---
+++ plagmatic-terraform/4-16/store.tf
@@ -0,0 +1,24 @@
+resource "aws_db_parameter_group" "example" {
+  name   = "example"
+  family = "mysql5.7"
+
+  parameter {
+    name  = "character_set_database"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+}
+
+resource "aws_db_option_group" "example" {
+  name                 = "example"
+  engine_name          = "mysql"
+  major_engine_version = "5.7"
+
+  option {
+    option_name = "MARIADB_AUDIT_PLUGIN"
+  }
+}
